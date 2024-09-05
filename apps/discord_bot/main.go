ef6466c00047c63266000545d2103b05a0f203f7
Dominic Nidy <dominicnidy3@gmail.com>
2024-09-05 18:38:11 -0400

    Implement 'hello world' for bot, working on fixing missing certs in bot runner

--- apps/discord_bot/main.go
+++ apps/discord_bot/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/DomNidy/saint_sim/apps/discord_bot/constants"
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
@@ -12,4 +17,31 @@ func main() {
 	fmt.Printf("%s: %s\n", constants.DBUser.Key(), constants.DBUser.MaskedValue())
 	fmt.Printf("%s: %s\n", constants.DBPassword.Key(), constants.DBPassword.MaskedValue())
 	fmt.Printf("%s: %s\n", constants.DiscordToken.Key(), constants.DiscordToken.MaskedValue())
+
+	discord, err := discordgo.New("Bot " + constants.DiscordToken.Value())
+	if err != nil {
+		log.Fatalf("Error occured during discord session creation: %v", err)
+		return
+	}
+	// Add event listener to respond to commmands
+	discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+		if m.Content == "!hello" {
+			s.ChannelMessageSend(m.ChannelID, "Hello, "+m.Author.Username+"!")
+		}
+	})
+
+	// Open websocket connection to discord
+	err = discord.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+	defer discord.Close()
+
+	fmt.Println("Bot is now running. Press CTRL+C to exit.")
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
 }
