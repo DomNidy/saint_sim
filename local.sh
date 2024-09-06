2f9ae09b9a87543b544fddae5eff3c5cf57784a3
Dominic Nidy <dominicnidy3@gmail.com>
2024-09-06 13:27:41 -0400

    improved comments, updated gitignore

--- local.sh
+++ local.sh
@@ -4,15 +4,14 @@ mode=$1
 
 if [[ "$mode" == "start" ]]; then
     echo "Building Docker images..."
-    docker-compose build
+    docker compose build
 
     echo "Starting saint_sim containers..."
-    docker-compose up
+    docker compose up
 
-    echo "Local environment is ready!"
 elif [[ "$mode" == "stop" ]]; then
     echo "Stopping saint_sim containers..."
-    docker-compose down
+    docker compose down
 else
     echo "Invalid mode specified. Please specify 'start' or 'stop' in the first argument"
 fi
