79dba673ba98fcad3df685d4b4eafa75dca76db7
DomNidy <dominicnidy3@gmail.com>
2024-09-16 15:15:15 -0400

    Work on consuming messages

--- apps/simulation_worker/go.mod
+++ apps/simulation_worker/go.mod
@@ -2,6 +2,10 @@ module github.com/DomNidy/saint_sim/apps/simulation_worker
 
 go 1.21.6
 
-require github.com/DomNidy/saint_sim/pkg/secrets v0.0.0-20240908203648-5e532d643359
+require (
+	github.com/DomNidy/saint_sim/pkg/secrets v0.0.0-20240908203648-5e532d643359
+	github.com/DomNidy/saint_sim/pkg/utils v0.0.0-20240916175438-8a28b6c09826
+	github.com/rabbitmq/amqp091-go v1.10.0
+)
 
 require github.com/joho/godotenv v1.5.1 // indirect
--- apps/simulation_worker/main.go
+++ apps/simulation_worker/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"time"
 
-	"github.com/DomNidy/saint_sim/pkg/secrets"
+	interfaces "github.com/DomNidy/saint_sim/pkg/interfaces"
+	secrets "github.com/DomNidy/saint_sim/pkg/secrets"
+	utils "github.com/DomNidy/saint_sim/pkg/utils"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func performSim(region, realm, name string) {
+
 	simcBinaryPath := secrets.LoadSecret("SIMC_BINARY_PATH")
 	outputFilePath := fmt.Sprintf("output=%v-%v-%v-%d.txt", region, realm, name, time.Now().Unix())
 	simCommand := exec.Cmd{
@@ -29,5 +34,65 @@ func performSim(region, realm, name string) {
 // todo: consume messages from rabbitmq queue
 // todo: write the results of the sim to database
 func main() {
-	performSim("us", "hydraxis", "ishton")
+	RABBITMQ_USER := secrets.LoadSecret("RABBITMQ_USER")
+	RABBITMQ_PASS := secrets.LoadSecret("RABBITMQ_PASS")
+	RABBITMQ_PORT := secrets.LoadSecret("RABBITMQ_PORT")
+	RABBITMQ_HOST := secrets.LoadSecret("RABBITMQ_HOST")
+	connectionURI := fmt.Sprintf("amqp://%s:%s@%s:%s", RABBITMQ_USER.Value(), RABBITMQ_PASS.Value(), RABBITMQ_HOST.Value(), RABBITMQ_PORT.Value())
+	// Connect to rabbitmq
+	conn, err := amqp.Dial(connectionURI)
+	utils.FailOnError(err, "Failed to connect to rabbitmq")
+	defer conn.Close()
+
+	// setup channel
+	ch, err := conn.Channel()
+	utils.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// declare queue
+	q, err := ch.QueueDeclare(
+		"simulation_queue", // name
+		false,              // durable
+		false,              // delete when unused
+		false,              // exclusive
+		false,              // no-wait
+		nil,                // arguments
+	)
+	utils.FailOnError(err, "Failed to declare a queue")
+
+	// Immediately start receiving queued messages
+	msgs, err := ch.Consume(
+		q.Name,
+		"",    // consumer
+		true,  // auto ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,   // args
+	)
+	utils.FailOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+	var receivedCount uint64
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s\n", d.Body)
+			log.Printf("receivedCount = %d\n", receivedCount)
+
+			var simRequestMsg interfaces.SimulateJSONRequestBody
+
+			err := json.Unmarshal(d.Body, &simRequestMsg)
+			if err != nil {
+				log.Printf("error unmarshalling json: %v", err)
+			}
+			fmt.Println("Received simulation request:")
+			fmt.Printf("	character_name: %s", *simRequestMsg.WowCharacter.CharacterName)
+			fmt.Printf("	realm: %s", *simRequestMsg.WowCharacter.Realm)
+			fmt.Printf("	region: %s", *simRequestMsg.WowCharacter.Region)
+
+			receivedCount += 1
+		}
+	}()
+	<-forever
 }
